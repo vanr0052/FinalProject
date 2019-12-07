eef1fbb96350629f33240d53752b8f6721638c5e
joe vanrompaey <vanr0052@algonquinlive.com>
2019-12-07 16:50:15 -0500

    connect to vm on azure

--- final.sh
+++ final.sh
@@ -7,3 +7,4 @@ git commit -m "first automated git commit"
 curl -u "vanr0052" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository", "private":true}'
 git remote add origin https://github.com/vanr0052/FinalProjectRepository.git
 git push -u origin master
+ssh joseph@52.147.208.158

419d20c393f68e1b97b8b9407b9778489700faab
joe vanrompaey <vanr0052@algonquinlive.com>
2019-12-07 16:04:28 -0500

    added origin and push

--- final.sh
+++ final.sh
@@ -5,3 +5,5 @@ cp -a /Library/WebServer/Documents/.  .
 git add -A
 git commit -m "first automated git commit"
 curl -u "vanr0052" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository", "private":true}'
+git remote add origin https://github.com/vanr0052/FinalProjectRepository.git
+git push -u origin master

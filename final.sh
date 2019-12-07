97f023f536a3c49f920c79c7d6fb733724b78777
joe vanrompaey <vanr0052@algonquinlive.com>
2019-12-07 15:58:32 -0500

    created repo on github

--- final.sh
+++ final.sh
@@ -4,3 +4,4 @@ git init
 cp -a /Library/WebServer/Documents/.  .
 git add -A
 git commit -m "first automated git commit"
+curl -u "vanr0052" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository", "private":true}'

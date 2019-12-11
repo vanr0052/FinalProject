f736cc53b718b706c60f12ecd0a20a643a5d3429
joe vanrompaey <vanr0052@algonquinlive.com>
2019-12-11 16:03:05 -0500

    combine files

--- final.sh
+++ final.sh
@@ -18,3 +18,15 @@ open -a Firefox http://vanr0052.hopto.org/
 /Users/joe/Library/Android/sdk/emulator/emulator -avd Nexus_10_API_25 &
 cd /Users/joe/Library/Android/sdk/platform-tools
 ./adb shell am start -a android.intent.action.VIEW -d 'http://vanr0052.hopto.org/'
+echo "FROM ubuntu:latest
+RUN apt update && apt install -y nginx && apt install unzip
+COPY Archive.zip  /var/www/html
+WORKDIR /var/www/html
+RUN unzip Archive.zip
+CMD [\"/usr/sbin/nginx\", \"-g\" , \"daemon off;\"]
+EXPOSE 80">> Dockerfile
+docker build . -t finalproject:v1
+docker run -p 1234:80 finalproject:v1
+xcrun simctl boot 3312C8D4-81B4-4BD7-9F1F-5519F9D2A338
+open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
+xcrun simctl openurl booted "http://localhost:1234"
